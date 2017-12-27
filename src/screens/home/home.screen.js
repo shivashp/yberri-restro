2265112b9504da268928312a6d2f8bd4008f2fd7
shivashp <shivapandey04@gmail.com>
2017-12-27 15:28:17 +0545

    Tabbar

--- src/screens/home/home.screen.js
+++ src/screens/home/home.screen.js
@@ -4,19 +4,34 @@ import {
 } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 import { ProfileScreen, LoginScreen, LocationScreen } from 'screens';
+import { Ionicons, FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
 
 const TabBar = props => {
+  const {tintColor} = props;
+  const active = props.navigationState.index;
   return (
     <View style={styles.bar}>
       <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("Profile")}>
-        <Text style={styles.buttonText}>Profile</Text>
+        <FontAwesome name="user-circle-o" size={18} style={[styles.tabIcon, {color: (active != 0)?'#AFB6BE':'#409DEF'}]}/>
+        <Text style={[styles.buttonText, {color: (active != 0)?'#AFB6BE':'#409DEF'}]}>Profile</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("Location")}>
-        <Text style={styles.buttonText}>Location</Text>
+        <MaterialCommunityIcons name="truck-delivery" size={18} style={[styles.tabIcon, {color: (active != 1)?'#AFB6BE':'#409DEF'}]}/>
+        <Text style={[styles.buttonText, {color: (active != 1)?'#AFB6BE':'#409DEF'}]}>Location</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("Setting")}>
+        <MaterialCommunityIcons name="truck-delivery" size={18} style={[styles.tabIcon, {color: (active != 2)?'#AFB6BE':'#409DEF'}]}/>
+        <Text style={[styles.buttonText, {color: (active != 2)?'#AFB6BE':'#409DEF'}]}>Settings</Text>
       </TouchableOpacity>
     </View>
   )
 }
+
+const SettingScreen = () => (
+  <View style={{flex: 1, backgroundColor: '#2A3A50', justifyContent: 'center', alignItems: 'center'}}>
+    <Text style={{color: 'white', fontSize: 17}}>Setting Screen</Text>
+  </View>
+);
 
 const styles = {
   bar: {
@@ -28,6 +43,14 @@ const styles = {
     elevation: 1,
     justifyContent: 'space-around',
     alignItems: 'center'
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {    
+    fontSize: 12,
+    fontFamily: 'open-sans'
   }
 }
 
@@ -38,11 +61,17 @@ const HomeScreen = TabNavigator({
   Location: {
     screen: LocationScreen,
   },
+  Setting: {
+    screen: SettingScreen,
+  },
 }, {
   tabBarPosition: 'bottom',
   swipeEnabled:false,
   animationEnabled: true,
-  tabBarComponent: TabBar
+  tabBarComponent: TabBar,
+  tabBarOptions: {
+    activeTintColor: '#409DEF',
+  },
 });
 
 
--- src/screens/location/location.screen.js
+++ src/screens/location/location.screen.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {
-  View, Text, StyleSheet, Platform, Alert
+  View, Text, StyleSheet, Platform, Alert, TextInput, TouchableOpacity
 } from 'react-native';
 import { MapView, Constants, Location, Permissions } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
 
 class LocationScreen extends React.Component {
   state = {
@@ -15,7 +16,7 @@ class LocationScreen extends React.Component {
     errorMessage: null,
   };
 
-  componentDidMount() {
+  componentWillMount() {
     if (Platform.OS === 'android' && !Constants.isDevice) {
       this.setState({
         errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
@@ -38,29 +39,37 @@ class LocationScreen extends React.Component {
       ...this.state.region, 
       latitude: location.coords.latitude,
       longitude: location.coords.longitude
-    }});
-  };
-  onRegionChange = (region) => {
-    this.setState({ region });
-  }
+    }});    
+    Alert.alert("Location", JSON.stringify(location)); 
+  };  
+  onRegionChange = region => this.setState({region})
   render() {
     let { location } = this.state;
     return (
       <View style={styles.container}>
         <MapView
-          style={{ flex: 1 }}
+          style={styles.map}
           onRegionChange={this.onRegionChange}
+          initialRegion = {this.state.region}
           region={this.state.region}
-        >
-          {/* <MapView.Marker
-            coordinate={{
-              latitude: this.state.region.latitude,
-              longitude: this.state.region.longitude
-            }}
-            title="Your Location"
-            description="Set pickup location"
-          /> */}
-        </MapView>
+        />   
+        <View style={styles.markerBg}>
+          <TouchableOpacity style={styles.marker}>
+            <Text style={styles.markerText}>SET DROP LOCATION</Text>
+          </TouchableOpacity>
+        </View>     
+        <View style={styles.topBar}>
+          <Ionicons name="ios-search" size={22} style={{marginTop: 3}} color="#409DEF" />
+          <TextInput
+            style={styles.input}
+            onChangeText={address => this.setState({address})}    
+            placeholder="Search your location..."            
+            placeholderTextColor="#AFB6BE"
+            selectionColor = "#409DEF"
+            returnKeyType = "search"            
+            underlineColorAndroid="transparent"          
+          />           
+          </View>
       </View>
     );
   }
@@ -69,7 +78,48 @@ class LocationScreen extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    position: 'relative'
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    zIndex: 1
+  },
+  markerBg: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',    
+    zIndex: 0, // Change zindex to make marker visible
+    justifyContent: 'center',
+    alignItems: 'center',     
+  },
+  marker: {
+    backgroundColor: '#0F0D1C',
+    borderRadius: 50,
+    zIndex: 3,
+  },
+  markerText: {
+    color: 'white',
+    paddingVertical: 6,
+    paddingHorizontal: 15
+  },
+  topBar: {
+    marginTop: 60,
+    marginHorizontal: 20,
+    padding: 10,
+    backgroundColor: '#fff',
+    elevation: 2,
+    flexDirection: 'row' 
+  },
+  input: {
+    flex: 1,
+    paddingLeft: 10
   }
 });
 
